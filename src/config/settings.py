8921545b340127dce7409b443b3d24ce80f366d0
Karun Agarwal <karun.agarwal@esmagico.in>
2023-03-01 14:51:07 +0530

    PostgreSQL Setup

--- src/config/settings.py
+++ src/config/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("POSTGRES_DATABASE_NAME"),
+        "USER": env("POSTGRES_DATABASE_USER"),
+        "PASSWORD": env("POSTGRES_DATABASE_PASSWORD"),
+        "HOST": env("POSTGRES_DATABASE_HOST"),
+        "PORT": env.int("POSTGRES_DATABASE_PORT"),
     }
 }
 
